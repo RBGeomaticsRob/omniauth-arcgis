fdd3036caf82b79fbafbc9c470299b17a7959545
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2024-04-11 00:00:25 +0100

    Update arcgis.rb

--- lib/omniauth/strategies/arcgis.rb
+++ lib/omniauth/strategies/arcgis.rb
@@ -38,6 +38,12 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/sharing/rest/portals/self', {:parse => :json, :params => {:f => 'json', :token => access_token.token}}).parsed
       end
+      
+      def token_params
+        super.tap do |params|
+          params.client_secret = options.client_secret
+        end
+      end
     end
   end
 end
